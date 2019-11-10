3ad3c4c1908517ee4a488d52a36fcbf657892922
Brett Haines <bhaines3@gatech.edu>
2019-11-09 23:36:05 -0500

    Adding initial game files

---
+++ game/scripts/player.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+class_name Player
+
+# Constants
+const MOVE_SPEED : int = 400
+const MOVE_ANGLE : float = 23.0
+
+# State variables
+var velocity : Vector2 = Vector2()
+
+func _physics_process(delta):
+	
+	var direction : Vector2 = Vector2(0, 0)
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+	
+	velocity.x = lerp(velocity.x, direction.x * MOVE_SPEED, 0.1)
+	velocity.y = lerp(velocity.y, direction.y * MOVE_SPEED, 0.1)
+	rotation = lerp_angle(rotation, deg2rad(direction.x * MOVE_ANGLE), 0.04)
+	move_and_collide(velocity * delta)
