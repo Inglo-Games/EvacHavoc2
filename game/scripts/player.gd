c0393bcc3d0d7788c87b4265ce8a9a4534e24053
Brett Haines <bhaines3@gatech.edu>
2019-11-10 11:44:51 -0500

    Replacing heli wheels with skis, adding propeller shapes

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -14,8 +14,10 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
+		direction.y -= 0.2
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
+		direction.y -= 0.2
 	if Input.is_action_pressed("move_down"):
 		direction.y += 1
 	if Input.is_action_pressed("move_up"):
