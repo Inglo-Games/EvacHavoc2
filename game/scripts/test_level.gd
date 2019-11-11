082864c27f9bcc15469f8a94cb571ecbd333a2f1
Brett Haines <bhaines3@gatech.edu>
2019-11-11 00:17:29 -0500

    Fixing fuel gauge in view

--- game/scripts/test_level.gd
+++ game/scripts/test_level.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 onready var player = $player
-onready var fuel_gauge = $gauge
+onready var fuel_gauge = $ui/gauge
 
 func _ready():
-	player.connect("remaining_fuel", $gauge, "_on_fuel_update")
+	player.connect("remaining_fuel", fuel_gauge, "_on_fuel_update")
