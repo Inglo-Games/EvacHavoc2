a6d7b687edc6982dcebaa74777749c713a020a6b
Brett Haines <bhaines3@gatech.edu>
2019-11-12 03:19:25 -0500

    Adding dialog window scene

---
+++ game/scripts/dialog.gd
@@ -0,0 +1,23 @@
+extends Control
+
+onready var label = $label
+
+var lines = []
+var current_line = 0
+
+func _ready():
+	set_process_input(true)
+	connect("gui_input", self, "_on_dialog_clicked")
+
+func _on_dialog_clicked(ev):
+	if ev is InputEventMouseButton and not ev.pressed:
+		if current_line < len(lines) - 1:
+			current_line += 1
+			label.text = lines[current_line]
+		else:
+			queue_free()
+
+func set_lines(string_array):
+	lines = string_array
+	current_line = 0
+	label.text = lines[current_line]
