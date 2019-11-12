a8ad50489899ce1d0b5a16090370002dce569f94
Brett Haines <bhaines3@gatech.edu>
2019-11-12 15:45:23 -0500

    Adding level template

---
+++ game/scripts/level_template.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+var PausePopup = preload("res://scripts/pause_popup.gd")
+
+onready var player = $player
+onready var fuel_gauge = $ui/gauge
+
+var dialogue_text = []
+
+func _ready():
+	player.connect("remaining_fuel", fuel_gauge, "_on_fuel_update")
+	for person in get_tree().get_nodes_in_group("people"):
+		person.connect("person_saved", self, "_on_person_saved")
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		_pause_game()
+
+func _on_person_saved():
+	var people = get_tree().get_nodes_in_group("people")
+	if len(people) == 1:
+		print("You win!")
+		get_tree().quit()
+
+func _pause_game():
+	var pause_menu = PausePopup.instance()
+	add_child(pause_menu)
+	pause_menu.popup_centered()
+	pause_menu.connect("quit_game", self, "_on_quit_game")
+	get_tree().set_pause(true)
+
+func _on_quit_game():
+	get_tree().quit()
