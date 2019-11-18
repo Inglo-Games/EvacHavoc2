e3bd00a67a30eeeb200eb609225d46c52a46b9b9
Brett Haines <bhaines3@gatech.edu>
2019-11-17 21:15:07 -0500

    Fixing pause menu

--- game/scripts/level_template.gd
+++ game/scripts/level_template.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-var PausePopup = preload("res://scripts/pause_popup.gd")
+var PausePopup = preload("res://scenes/pause_popup.tscn")
+var DialogWindow = preload("res://scenes/dialog.tscn")
 
 export var dialogue_text = []
 export var next_scene = "res://scenes/menu.tscn"
@@ -13,19 +14,21 @@ func _ready():
 	
 	_on_person_saved()
 	if dialogue_text != []:
-		get_tree().paused = true
-		$ui/dialog.set_lines(dialogue_text)
-		$ui/dialog.visible = true
+		var dialog = DialogWindow.instance()
+		$ui.add_child(dialog)
+		dialog.set_lines(dialogue_text)
+		get_tree().set_pause(true)
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		_pause_game()
 
 func _pause_game():
-	var pause_menu = PausePopup.new()
+	var pause_menu = PausePopup.instance()
 	$ui.add_child(pause_menu)
-	pause_menu.popup_centered()
+	pause_menu.pause_mode = Node.PAUSE_MODE_PROCESS
 	pause_menu.connect("quit_game", self, "_on_quit_game")
+	pause_menu.popup_centered()
 	get_tree().set_pause(true)
 
 func _on_quit_game():
