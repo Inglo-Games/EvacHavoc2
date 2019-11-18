f24a9e580744f9c650b95499725b6655500f2a78
Brett Haines <bhaines3@gatech.edu>
2019-11-17 23:35:38 -0500

    Making music play during pause

--- game/scripts/bgm.gd
+++ game/scripts/bgm.gd
@@ -4,7 +4,8 @@ onready var bgm_player : AudioStreamPlayer = AudioStreamPlayer.new()
 onready var stream : AudioStreamOGGVorbis = preload("res://assets/sfx/RevvedUp.ogg")
 
 func _ready():
-	bgm_player.volume_db = -18.5
+	bgm_player.pause_mode = Node.PAUSE_MODE_PROCESS
+	bgm_player.volume_db = -14.5
 	bgm_player.set_stream(stream)
 	get_tree().get_root().call_deferred("add_child", bgm_player)
 	bgm_player.play()
