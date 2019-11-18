40798cf194e86484a8c507207e9e60a60f50875e
Brett Haines <bhaines3@gatech.edu>
2019-11-17 22:53:14 -0500

    Getting dialog input manually

--- game/scripts/dialog.gd
+++ game/scripts/dialog.gd
@@ -1,20 +1,20 @@
 extends Control
 
-onready var label = $label
-
 var lines = []
 var current_line = 0
 
 func _ready():
 	set_process_input(true)
-	connect("gui_input", self, "_on_dialog_clicked")
+	connect("gui_input", self, "_on_dialog_event")
 
-func _on_dialog_clicked(ev):
-	if ev is InputEventMouseButton and not ev.pressed:
-		print("Dialog clicked...")
+func _input(event):
+	_on_dialog_event(event)
+
+func _on_dialog_event(ev):
+	if (ev is InputEventMouseButton or ev is InputEventKey) and not ev.pressed:
 		if current_line < len(lines) - 1:
 			current_line += 1
-			label.text = lines[current_line]
+			$label.text = lines[current_line]
 		else:
 			get_tree().paused = false
 			queue_free()
@@ -22,4 +22,4 @@ func _on_dialog_clicked(ev):
 func set_lines(string_array):
 	lines = string_array
 	current_line = 0
-	label.text = lines[current_line]
+	$label.text = lines[current_line]
